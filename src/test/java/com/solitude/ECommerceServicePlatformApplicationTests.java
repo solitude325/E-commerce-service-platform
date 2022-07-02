b913f8fe28482225bb1243ae5f97c3652e1f9315
solitude325 <xianke0325521@163.com>
2022-07-02 11:23:11 +0800

    init project

---
+++ src/test/java/com/solitude/ECommerceServicePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solitude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceServicePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
